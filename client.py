7b7820879f40210fe852c253404e5c2b030e73d6
PirlouitDumez <71868243+PirlouitDumez@users.noreply.github.com>
2022-11-18 00:18:12 +0100

    Add files via upload
    jai avance sur le menu + utilise la colonne available, faut encore l'ajouter pour la desactivation/reactivation par areas

--- client.py
+++ client.py
@@ -1,104 +1,27 @@
-def find_all(db, affichage=True):
-    # filtre all stations
-    found = db.stations.find({})
-    found = list(found)
-    
-    if not affichage:
-        return found
-    
-    if len(found)>0:
-        # get datas row corresponding to id
-        for f in found:
-            found_data = list(db.datas.find({"station_id" : f["_id"]}).sort([("date", -1)]).limit(1))[0]
-            print(f"Station {f['_id']}: {f['name']} : {found_data['bike_availbale']}/{f['size']} ({found_data['date']})")
-            print(f"position : {f['geometry']['coordinates']}")
-            print('\n')
+import pprint
+from business import activate_station,desactivate_station
+
+def location_program(db,pos,dist=100):
+    """
+    find all the stations in a certain radius from the user
+    """
+    # filtre nearby dans global
+    found= db.stations.find({"Geo": 
+                                   {"$near": 
+                                    {"$geometry":
+                                     {"type": "Point",
+                                      "coordinates": pos},
+                                                     "$maxDistance": dist}}, "Available":"True"})
+
+    if len(list(found))>0: #jai eu un bug en utilisant la longueur des listes comme ca pcq comme faut les pprint je crois elles sont bizarres
+        # for each station found, we look for stations in lille by id
+        print("Stations found:")
+        for f in found:        
+
+            foundLille = db.datas.find({"idstation" : f["_id"]}).sort([("datemaj", -1)]).limit(1)
+
+
+            for fl in foundLille:
+                pprint.pprint(fl)
     else:
-        print("No station in area")
-        
-    return found
-
- 
-
-
-def find_in_area(db,pos,dist=100,affichage=True):
-    # find stations in circle around position
-    db.stations.create_index([('geometry','2dsphere')])
-    found = db.stations.find({'geometry': {'$near': {
-                                                '$geometry': {
-                                                    'type': "Point" ,
-                                                    'coordinates': pos},
-                                                 "$maxDistance": dist
-                                                }}})
-
-    found = list(found)
-    
-    if not affichage:
-        return found
-    
-    if len(found)>0:
-        # get datas row corresponding to id
-        for f in found:
-            found_data = list(db.datas.find({"station_id" : f["_id"]}).sort([("date", -1)]).limit(1))[0]
-            print(f"Station {f['_id']}: {f['name']} : {found_data['bike_availbale']}/{f['size']} ({found_data['date']})")
-            print(f"position : {f['geometry']['coordinates']}")
-            print('\n')
-    else:
-        print("No station in area")
-        
-    return found
-
-
-
-
-def find_in_polygon(db, coordinates, affichage=True):
-    # find stations in polygon 
-    db.stations.create_index([('geometry','2dsphere')])
-    found = db.stations.find({'geometry': {'$geoWithin': {
-                                                '$geometry': {
-                                                    'type': "Polygon" ,
-                                                    'coordinates': coordinates}}}})
-
-    found = list(found)
-    
-    if not affichage:
-        return found
-    
-    if len(found)>0:
-        # get datas row corresponding to id
-        for f in found:
-            found_data = list(db.datas.find({"station_id" : f["_id"]}).sort([("date", -1)]).limit(1))[0]
-            print(f"Station {f['_id']}: {f['name']} : {found_data['bike_availbale']}/{f['size']} ({found_data['date']})")
-            print(f"position : {f['geometry']['coordinates']}")
-            print('\n')
-    else:
-        print("No station in area")
-        
-    return found
-
-
-
-
-
-if __name__ == '__main__':
-    from pymongo.mongo_client import MongoClient
-    from pymongo.server_api import ServerApi
-    from get_data import *
-    
-    # Create DB object
-    client = MongoClient("mongodb+srv://db:1112@cluster0.hscucjd.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-    db = client.vls
-    
-    update(db)
-        
-    position=[3.048567, 50.634268]
-    stations = find_all(db, affichage=False)
-    print(len(stations))
-    
-    stations = find_in_area(db, position, dist=300, affichage=False)
-    print(len(stations))
-    
-    alpha = 0.005
-    coords = [[[3.048567+alpha, 50.634268], [3.048567, 50.634268+alpha], [3.048567-alpha, 50.634268], [3.048567, 50.634268-alpha], [3.048567+alpha, 50.634268]]]
-    stations = find_in_polygon(db, coords, affichage=True)
-    print(len(stations))
+        print("No station found")
--- main_2.py
+++ main_2.py
@@ -16,7 +16,81 @@ if __name__ == '__main__':
     #while True:
     ### Update db
     update(db)
-    
+    running=True
+    while running:
+        print("Choose an action : \n 1 - Give station next to user \n 2 - Search station by name \n 3 - Update a station \n 4"
+              + " - Delete a station \n 5 - Desactivate all station in an area\n 6 -  Give all stations with a ratio bike/total_stand under 20%\n Other entry - Exit " +
+              " \n -------------------------------------------------------------------------------------------------------------")
+        try:
+            choice = int(input())
+        except:
+            print("Thanks for using our application, see you soon :) ")
+            running=False
+            break
+        if choice==1: #give station nearby
+            pass
+        
+        elif choice==2: #search by name
+            print("Enter the name of the station (or some letters)")
+            name = input()
+            search_station_by_name(db,name)
+            
+        elif choice==3: #update a station
+            print("Enter the station ID to modify (use search by name option to get the ID)")
+            try:
+                id = int(input())
+            except:
+                print("Wrong entry please enter an integer")
+                break
+            updating=True
+            fields = []
+            values = []
+            while updating:
+                station = db.stations.find({"_id": id})
+                print("Here is information of this station : \n")
+                pprint.pprint(list(station))
+                print("Enter the field to modify : ")
+                field = input()
+                print("Enter the new value of ",field)
+                value = input()
+                fields.append(field)
+                values.append(value)
+                print("Do you want to modify another field ? Enter y if yes or anything else if no : ")
+                continue_choice = input()
+                if continue_choice != "y":
+                    updating=False
+            
+            modif_dict = {field:value for field,value in zip(fields,values)} 
+            update_station(db,id,modif_dict)
+            print(f"Station {id} has been successfully updated !")
+            
+        elif choice==4: #delete station
+            print("Enter the station ID to delete (use search by name option to get the ID)")
+            try:
+                id = int(input())
+            except:
+                print("Wrong entry")
+                break
+            
+            print("Here is information of this station : \n Press d to confirm the suppression : ")
+            pprint.pprint(list(db.stations.find({"_id": id})))
+            delete_choice = input()
+            if delete_choice == "d":
+                delete_station(db,id)
+                print(f"Station {id} has been successfully deleted")
+            else:
+                print("Suppression canceled")
+        elif choice==5: #desactivate in area
+            pass
+        elif choice==6: #give all stations with ratio bike/total_stand under 20%
+            pass
+        else:
+            running=False
+            print("Thanks for using our application, see you soon :) ")
+           
+            
+            
+    """    
     #for station in db.stations.find():
     #    pprint.pprint(list(station))
     desactivate_station(db,240)
@@ -29,4 +103,4 @@ if __name__ == '__main__':
     #position=[3.068022,50.631750][::-1]
     #location_program(db, position, dist=1000)
     
-    
+    """
