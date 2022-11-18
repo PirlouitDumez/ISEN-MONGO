9e49e56cbd55c815d8909367f9911692daa572c6
Guillaume Prévost <71766101+Prevost-Guillaume@users.noreply.github.com>
2022-11-18 13:47:13 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
-def find_all(db, affichage=True):
+from business import *
+
+
+def find_all(db, affichage=True, only_available=True):
     # filtre all stations
-    found = db.stations.find({})
+    if only_available:
+        found = db.stations.find({"Available":True})
+    else:
+        found = db.stations.find({})
     found = list(found)
     
     if not affichage:
@@ -21,16 +27,18 @@ def find_all(db, affichage=True):
  
 
 
-def find_in_area(db,pos,dist=100,affichage=True):
+def find_in_area(db,pos,dist=100,affichage=True, only_available=True):
     # find stations in circle around position
     db.stations.create_index([('geometry','2dsphere')])
-    found = db.stations.find({'geometry': {'$near': {
-                                                '$geometry': {
-                                                    'type': "Point" ,
-                                                    'coordinates': pos},
-                                                 "$maxDistance": dist
-                                                }}})
-
+    if only_available:
+        found = db.stations.find({'geometry': {'$near': {
+                                                '$geometry': {'type': "Point" ,'coordinates': pos},
+                                                 "$maxDistance": dist}},"Available":True})
+    else:
+       found = db.stations.find({'geometry': {'$near': {
+                                                '$geometry': {'type': "Point" ,'coordinates': pos},
+                                                 "$maxDistance": dist}}})
+       
     found = list(found)
     
     if not affichage:
@@ -51,13 +59,16 @@ def find_in_area(db,pos,dist=100,affichage=True):
 
 
 
-def find_in_polygon(db, coordinates, affichage=True):
+def find_in_polygon(db, coordinates, affichage=True, only_available=True):
     # find stations in polygon 
     db.stations.create_index([('geometry','2dsphere')])
-    found = db.stations.find({'geometry': {'$geoWithin': {
-                                                '$geometry': {
-                                                    'type': "Polygon" ,
-                                                    'coordinates': coordinates}}}})
+    if only_available:
+        found = db.stations.find({'geometry': {'$geoWithin': {
+                                                '$geometry': {'type': "Polygon" ,'coordinates': coordinates}}},
+                                  "Available":True})
+    else:
+        found = db.stations.find({'geometry': {'$geoWithin': {
+                                                '$geometry': {'type': "Polygon" ,'coordinates': coordinates}}}})
 
     found = list(found)
     
@@ -76,6 +87,35 @@ def find_in_polygon(db, coordinates, affichage=True):
         
     return found
 
+
+
+def desactivate_in_area(db,pos,dist=100):
+    # desactivate stations in circle around pos
+    stations = find_in_area(db,pos,dist=dist,affichage=False)
+    for s in stations:
+        desactivate_station(db, s['_id'])
+    return
+
+def desactivate_in_polygon(db,coordinates):
+    # desactivate stations in circle around pos
+    stations = find_in_polygon(db,coordinates,affichage=False)
+    for s in stations:
+        desactivate_station(db, s['_id'])
+    return
+
+def activate_in_area(db,pos,dist=100):
+    # desactivate stations in circle around pos
+    stations = find_in_area(db,pos,dist=dist,affichage=False, only_available=False)
+    for s in stations:
+        activate_station(db, s['_id'])
+    return
+
+def activate_in_polygon(db,coordinates):
+    # desactivate stations in circle around pos
+    stations = find_in_polygon(db,coordinates,affichage=False, only_available=False)
+    for s in stations:
+        activate_station(db, s['_id'])
+    return
 
 
 
